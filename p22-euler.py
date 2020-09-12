a0853b52ca5c4b35fc40eff954554351028ee41b
Christian Kischy Haege <kischy@gmx.de>
2020-09-12 15:37:28 +0200

    Added basic files

---
+++ p22-euler.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Aug 16 12:20:22 2020
+
+@author: kisch
+"""
+
+
+
+problem_number = 22
+
+
+print("Calculation started")
+
+
+
+
+the_answer = 0
+
+print("The answer to the " + str(problem_number) + "th problem of ProjectEuler.Net is:",the_answer)
+
+
---
+++ p22-tests.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Aug 16 12:20:22 2020
+
+@author: kisch
+"""
+
+import unittest
+
+
+
+class TestSomething(unittest.TestCase):  
+    
+    def setUp(self):
+        pass
+        
+    def test_get_last_digit(self):     
+        self.assertEqual(1,1)
+        
+        
+
+
+
+if __name__ == '__main__':
+    unittest.main(verbosity=0)
